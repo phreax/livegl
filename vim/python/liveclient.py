57fd5aafa645daf3862014151ff67bf08d700fe3
Michael Thomas <m.thomas@networksec.de>
2012-10-27 03:23:17 +0200

    Add feature:     - parse shader for requirements     - load shader modules     - Example: // require "toobox.glsl"

--- vim/python/liveclient.py
+++ vim/python/liveclient.py
@@ -1,6 +1,7 @@
 import vim
 import zmq
 import time
+import re
 
 class LiveClient:
     
@@ -11,8 +12,9 @@ class LiveClient:
 
         self.socket.connect(endpoint)
         self.t = time.time()
-
         self.active = True
+
+        self.requireRegex = re.compile('^//\s*require\s+"([\w,./]+)"\s*$',re.MULTILINE)
 
     def send_shader(self,shader_type):
 
@@ -21,12 +23,12 @@ class LiveClient:
         now = time.time()
         if(now-self.t > 1):
             source = '\n'.join(vim.current.buffer)
+            source = self.include_requirements(source)
             msg.update({"shader":shader_type,"source":source})
             try:
                 self.socket.send_json(msg,zmq.NOBLOCK)
             except zmq.ZMQError:
                 print "Server not ready"
-
 
         self.t = now
      
@@ -66,6 +68,19 @@ class LiveClient:
         else:
             self.resume_shader()
 
+
+    def include_requirements(self,src):
+
+        def match_function(match):
+            filename = match.group(1)
+            try:
+                f = file(filename)
+                return f.read()
+            except:
+                print 'Could not open file: '+filename
+                return ""
+
+        return self.requireRegex.sub(match_function,src)
 
         
 if __name__ == "__main__":
